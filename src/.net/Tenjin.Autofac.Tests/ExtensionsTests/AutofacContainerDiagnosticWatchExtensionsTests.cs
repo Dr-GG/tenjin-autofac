15a4d07f8ee27cb58ced6fa5efe6b1422096bf57
Gerard Gouws <gerard.gouws@capgemini.com>
2022-05-18 10:01:24 +0200

    Tenjin Diagnostics watch implementation
    20220518 - Added default Autofac integration for the Tenjin IDiagnosticsWatch and ISystemClockProvider interfaces.

---
+++ src/.net/Tenjin.Autofac.Tests/ExtensionsTests/AutofacContainerDiagnosticWatchExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Autofac;
+using NUnit.Framework;
+using Tenjin.Autofac.Extensions;
+using Tenjin.Implementations.Diagnostics;
+using Tenjin.Interfaces.Diagnostics;
+
+namespace Tenjin.Autofac.Tests.ExtensionsTests
+{
+    [TestFixture]
+    public class AutofacContainerDiagnosticWatchExtensionsTests
+    {
+        [Test]
+        public void RegisterDiagnosticsWatch_WhenCalled_RegistersAISystemClockProviderInstance()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterDiagnosticsWatch();
+
+            using var container = containerBuilder.Build();
+            var clockProvider = container.Resolve<ISystemClockProvider>();
+
+            Assert.IsNotNull(clockProvider);
+            Assert.IsInstanceOf<ISystemClockProvider>(clockProvider);
+            Assert.IsInstanceOf<SystemClockProvider>(clockProvider);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void RegisterSystemClockProvider_WhenCalled_RegistersAISystemClockProviderInstance(bool useUtc)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterSystemClockProvider(useUtc);
+
+            using var container = containerBuilder.Build();
+            var clockProvider = container.Resolve<ISystemClockProvider>();
+            var now = useUtc ? DateTime.UtcNow : DateTime.Now;
+            var providedNow = clockProvider.Now();
+            var differenceSeconds = (providedNow - now).TotalMilliseconds;
+
+            Assert.LessOrEqual(differenceSeconds, 1.0);
+        }
+    }
+}
---
+++ src/.net/Tenjin.Autofac/Extensions/AutofacContainerDiagnosticWatchExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Tenjin.Implementations.Diagnostics;
+using Tenjin.Interfaces.Diagnostics;
+
+namespace Tenjin.Autofac.Extensions
+{
+    public static class AutofacContainerDiagnosticWatchExtensions
+    {
+        public static void RegisterSystemClockProvider(this ContainerBuilder builder, bool useUct = true)
+        {
+            builder
+                .Register(_ => new SystemClockProvider(useUct))
+                .As<ISystemClockProvider>()
+                .SingleInstance();
+        }
+
+        public static void RegisterDiagnosticsWatch(this ContainerBuilder builder, bool useUct = true)
+        {
+           builder.RegisterSystemClockProvider(useUct);
+
+            builder
+                .RegisterType<DiagnosticsStopwatch>()
+                .As<IDiagnosticsStopwatch>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
